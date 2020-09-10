967fc6ea6893c09f11cc40904ce5e685e58a29c1
Becca-Saka <Ree4sure@gmail.com>
2020-09-10 14:20:54 +0100

    final commit

--- app/src/main/java/com/gadspracticeproject/ProjectSubmission.java
+++ app/src/main/java/com/gadspracticeproject/ProjectSubmission.java
@@ -2,11 +2,11 @@ package com.gadspracticeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -23,9 +23,9 @@ public class ProjectSubmission extends AppCompatActivity {
     AppCompatButton submit;
     Dialog confirmationDialog;
     private static final String TAG = "ProjectSubmission";
-    EditText email,first ,last, link;
+    EditText email, first, last, link;
     ImageButton back;
-    String emailAddress, firstName,lastName, projectLink;
+    String emailAddress, firstName, lastName, projectLink;
     ProgressDialog progressDialog;
 
     @Override
@@ -38,42 +38,44 @@ public class ProjectSubmission extends AppCompatActivity {
 
 
         submit.setOnClickListener(view -> {
+            firstName = first.getText().toString();
+            lastName = last.getText().toString();
+            emailAddress = email.getText().toString();
+            projectLink = link.getText().toString();
             Log.d(TAG, "onClick: Clicked");
-            if(firstName.isEmpty()|| lastName.isEmpty()|| emailAddress.isEmpty()||projectLink.isEmpty()){
+            if (firstName.isEmpty() || lastName.isEmpty() || emailAddress.isEmpty() || projectLink.isEmpty()) {
                 Toast.makeText(this, "All fields are required", Toast.LENGTH_SHORT).show();
 
                 Log.d(TAG, "onClick: Empty");
 
-            }else{
+            } else {
                 showConfirmationDialog();
             }
 
 
-
-
         });
     }
-    private void setUpUi(){
-         back = findViewById(R.id.back);
-         email = findViewById(R.id.email);
-         first = findViewById(R.id.first_name);
-         last = findViewById(R.id.last_name);
-         link = findViewById(R.id.project_link);
+
+    private void setUpUi() {
+        back = findViewById(R.id.back);
+        email = findViewById(R.id.email);
+        first = findViewById(R.id.first_name);
+        last = findViewById(R.id.last_name);
+        link = findViewById(R.id.project_link);
         submit = findViewById(R.id.submit_button);
 
-       firstName= first.getText().toString();
-       lastName =last.getText().toString();
-       emailAddress=  email.getText().toString();
-       projectLink=  link.getText().toString();
-       progressDialog = new ProgressDialog(ProjectSubmission.this);
 
+        progressDialog = new ProgressDialog(ProjectSubmission.this, R.style.MyGravity);
+
+        progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
 
 
     }
-    private void showConfirmationDialog(){
+
+    private void showConfirmationDialog() {
         confirmationDialog = new Dialog(this);
         confirmationDialog.setContentView(R.layout.confirmation_dialog);
-        confirmationDialog.getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        confirmationDialog.getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         AppCompatButton confirmButton = confirmationDialog.findViewById(R.id.confirm_button);
         ImageButton cancel = confirmationDialog.findViewById(R.id.cancel_button);
         cancel.setOnClickListener(view -> confirmationDialog.dismiss());
@@ -89,38 +91,44 @@ public class ProjectSubmission extends AppCompatActivity {
         confirmationDialog.show();
 
     }
-    private void showSuccessDialog(){
+
+    private void showSuccessDialog() {
         Dialog dialog = new Dialog(this);
         dialog.setContentView(R.layout.success_dialog);
-        dialog.getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         dialog.show();
 
     }
-    private void showErrorDialog(){
+
+    private void showErrorDialog() {
         Dialog dialog = new Dialog(this);
         dialog.setContentView(R.layout.error_dialog);
-        dialog.getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         dialog.show();
 
     }
-    private void sendPostRequest(){
-       Retrofit retrofit = new Retrofit.Builder()
+
+    private void sendPostRequest() {
+        Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://docs.google.com/forms/d/e/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         Log.d(TAG, "onCreateView: Retrofit Gotten");
         ApiRepo apiRepo = retrofit.create(ApiRepo.class);
         Call<Void> call = apiRepo.submitForm(
-                firstName,lastName,emailAddress,projectLink
+                firstName, lastName, emailAddress, projectLink
         );
         call.enqueue(new Callback<Void>() {
             @Override
             public void onResponse(Call<Void> call, Response<Void> response) {
-                if(!response.isSuccessful()){
+                if (!response.isSuccessful()) {
                     progressDialog.dismiss();
                     showErrorDialog();
+                    Log.d(TAG, "onResponse: " + response.message());
+                    return;
                 }
 
+                progressDialog.dismiss();
                 showSuccessDialog();
             }
 
@@ -128,7 +136,7 @@ public class ProjectSubmission extends AppCompatActivity {
             public void onFailure(Call<Void> call, Throwable t) {
                 progressDialog.dismiss();
                 showErrorDialog();
-                Log.d(TAG, "onFailure:"+t.getMessage());
+                Log.d(TAG, "onFailure:" + t.getMessage());
 
             }
         });
